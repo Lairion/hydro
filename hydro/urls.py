b34de42cdc82d4daafe2c1e716686f5373158698
Lairion <lairion@github.com>
2021-10-02 12:36:56 +0300

    Add static root

--- hydro/urls.py
+++ hydro/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("hydro_capsule.api.urls"))
-]
+urlpatterns =(
+    [
+        path('admin/', admin.site.urls),
+        path('', include("hydro_capsule.api.urls"))
+        ]
+        + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)

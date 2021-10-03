deacdda038ca59743d0384c6b5915e3d6ce776e8
Manul <naivze@gmail.com>
2021-10-03 11:16:37 +0300

    Move index.html

--- hydro_capsule/views.py
+++ hydro_capsule/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "hydro_capsule\templates\hydro_capsule\index.html", {})
+    return render(request, "templates\index.html", {})

b7166b09e7bf681ec22bcf850b51b47f611efe44
Manul <naivze@gmail.com>
2021-10-21 01:13:45 +0300

    Update settings

--- hydro/settings_base.py
+++ hydro/settings_base.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'rest_framework',
     'corsheaders',
 
     'hydro_capsule',
+
 ]
 
 MIDDLEWARE = [
